1046f96bad64a11e7fb68aac0db65b2d122be7ef
Dean Johnson <jdean300@yahoo.com>
2016-04-07 11:05:08 -0700

    Added PixelToHex and PixelToWholeHex methods

--- SFHex/HexUtils.cpp
+++ SFHex/HexUtils.cpp
@@ -10,3 +10,25 @@ sf::Vector2f HexUtils::HexToPixel(const Hex& h, const Layout& l)
 
 	return sf::Vector2f(x + l.Origin.x, y + l.Origin.y);
 }
+
+FractionalHex HexUtils::PixelToHex(const sf::Vector2f& p, const Layout& l) 
+{
+	auto o = l.Orientation;
+	auto pt = sf::Vector2f((p.x - l.Origin.x) / l.Size.x, (p.y - l.Origin.y) / l.Size.y);
+
+	auto x = o.B0*pt.x + o.B1*pt.y;
+	auto y = o.B2*pt.x + o.B3*pt.y;
+
+	return FractionalHex(x, y);
+}
+
+Hex HexUtils::PixelToWholeHex(const sf::Vector2f& p, const Layout& l)
+{
+	auto o = l.Orientation;
+	auto pt = sf::Vector2f((p.x - l.Origin.x) / l.Size.x, (p.y - l.Origin.y) / l.Size.y);
+
+	auto x = o.B0*pt.x + o.B1*pt.y;
+	auto y = o.B2*pt.x + o.B3*pt.y;
+
+	return FractionalHex(x, y).Round();
+}
--- SFHex/HexUtils.h
+++ SFHex/HexUtils.h
@@ -4,11 +4,15 @@
 #define HEXUTILS_H
 
 #include "Hex.h"
+#include "FractionalHex.h"
 
 namespace HexUtils
 {
 	//Returns the center "pixel" of the given Hex
 	sf::Vector2f HexToPixel(const Hex& h, const Layout& l);
+
+	FractionalHex PixelToHex(const sf::Vector2f& p, const Layout& l);
+	Hex PixelToWholeHex(const sf::Vector2f& p, const Layout& l);
 }
 
 #endif
